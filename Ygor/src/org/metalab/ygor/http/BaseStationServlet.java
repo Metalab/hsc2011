a41c14f56e043503f9f6111bd787f1553c2ee36b
amir <amir@viel-zu.org>
2011-04-10 19:23:24 +0200

    base station servlet

---
+++ Ygor/src/org/metalab/ygor/http/BaseStationServlet.java
@@ -0,0 +1,30 @@
+package org.metalab.ygor.http;
+
+import java.io.OutputStream;
+
+import org.metalab.ygor.YgorDaemon;
+import org.metalab.ygor.YgorException;
+import org.metalab.ygor.db.YgorRequest;
+import org.metalab.ygor.serial.packet.Dispatcher;
+
+public class BaseStationServlet extends YgorServlet {
+  private Dispatcher dispatcher;
+  
+  public BaseStationServlet() {
+    super("text/html");
+    this.dispatcher = YgorDaemon.baseStation().getDispatcher();
+  }
+
+  protected void process(YgorRequest query, OutputStream out) {
+    String cmd = query.value("cmd");
+    try {
+      if (cmd == "accept_login") {
+        dispatcher.acceptLogin(query).writeJson(out);
+      } else {
+        YgorDaemon.baseStation().transmit(cmd);
+      }
+    } catch (Exception e) {
+      throw new YgorException("Unable to execute controller command: " + cmd, e);
+    }
+  }
+}
