0c7637cb225b869e0298c9f787b82d646dfb70e6
amir <amir@viel-zu.org>
2011-04-22 17:51:06 +0200

    ygor build

---
+++ Ygor/build.sh
@@ -0,0 +1,2 @@
+javac -d bin/ -sourcepath src/ -cp "lib/json_org.jar:lib/app.jar:lib/conn_chk.jar:lib/gnujsp09.jar:lib/jspengine.jar:lib/jsp.jar:lib/log4j-1.2.13.jar:lib/servlet-2-3.jar:lib/war.jar:lib/webserver.jar:lib/commons-codec-1.5.jar:lib/sqlitejdbc-v056.jar" src/org/metalab/ygor/YgorDaemon.java src/org/metalab/ygor/http/YgorWeb.java src/org/metalab/ygor/http/QueryServlet.java src/org/metalab/ygor/http/BaseStationServlet.java src/org/metalab/ygor/http/YgorServlet.java
+
