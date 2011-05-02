7e058def01195e2b63d9db3139c5d748fea03c33
chrysn <chrysn@fsfe.org>
2011-05-02 18:27:52 +0200

    tell where to find arduino-cc for build.sh

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-( cd vmcode && ./build.py; )
+# if you don'thave arduino-cc:
+# wget 'http://svn.clifford.at/tools/trunk/arduino-cc.sh' -O arduino-cc
+# chmod +x arduino-cc
+# export PATH=$PWD
+
+( cd vmcode && ./build.py && ./build-example.sh )
 arduino-cc firmware.cc hardware.cc RF12.cpp OneWire.cpp embedvm.c \
 	firmware_vm.cc firmware_evt.cc firmware_ser.cc firmware_net.cc
 arduino-cc ioexample.cc hardware.cc RF12.cpp OneWire.cpp
