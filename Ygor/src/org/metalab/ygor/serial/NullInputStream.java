61a400cdbf2299d8326f3c6ff68a88eee457a68f
amir <amir@viel-zu.org>
2011-05-05 19:20:29 +0200

    make null streams nop

--- Ygor/src/org/metalab/ygor/serial/NullInputStream.java
+++ Ygor/src/org/metalab/ygor/serial/NullInputStream.java
@@ -5,16 +5,11 @@ import java.io.InputStream;
 import java.util.concurrent.Semaphore;
 
 public class NullInputStream extends InputStream {
-  Semaphore blockUntilClose = new Semaphore(0);
   public int read() throws IOException {
-    try {
-      blockUntilClose.acquire();
-    } catch (InterruptedException e) { e.printStackTrace(); }
-    return -1;
+    return 0;
   }
   
   public void close() throws IOException {
-    blockUntilClose.release();
   }
 
 }
--- Ygor/src/org/metalab/ygor/serial/NullOutputStream.java
+++ Ygor/src/org/metalab/ygor/serial/NullOutputStream.java
@@ -6,16 +6,12 @@ import java.io.OutputStream;
 import java.util.concurrent.Semaphore;
 
 public class NullOutputStream extends OutputStream {
-  Semaphore blockUntilClose = new Semaphore(0);
   public void close() throws IOException {
-    blockUntilClose.release();
+
   }
 
-  @Override
+
   public void write(int b) throws IOException {
-    try {
-      blockUntilClose.acquire();
-    } catch (InterruptedException e) { e.printStackTrace(); }
-    throw new IOException("Nullstream closed");
+
   }
 }
